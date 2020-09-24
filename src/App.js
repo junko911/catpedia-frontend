35231ae1eb6387f474e3a75df6ed934f3ac01ab7
jakelozano94 <jakelozano94@gmail.com>
2020-09-24 03:31:23 -0400

    added a generic fetch request to create new user

--- src/App.js
+++ src/App.js
@@ -3,6 +3,27 @@ import './App.css';
 
 class App extends React.Component {
 
+componentDidMount(){
+
+
+  fetch('http://localhost:3000/api/v1/users', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json'
+  },
+  body: JSON.stringify({
+    user: {
+      username: "sylviawoods",
+      password: "whatscooking",
+      avatar: "https://upload.wikimedia.org/wikipedia/commons/4/49/Syvia_of_Sylvia%27s_reaturant_N.Y.C_%28cropped%29.jpg"
+    }
+  })
+})
+  .then(r => r.json())
+  .then(console.log)
+}
+
   render() {
     return <h1>App</h1>
   }
