4578b3bc32f29730645eeba35766d0dfd22b3d60
jakelozano94 <jakelozano94@gmail.com>
2020-09-25 13:40:10 -0400

    fixed index needed auth

--- src/Containers/CatContainer.js
+++ src/Containers/CatContainer.js
@@ -1,7 +1,24 @@
 import React from 'react'
 
-const CatContainer = () => {
-  return <div>CatContainer</div>
+class CatContainer extends React.Component {
+
+  componentDidMount(){
+    let token = localStorage.getItem("token")
+    fetch("http://localhost:3000/cats", {
+      method: "GET",
+      headers: {
+        "content-type": "application/json",
+        accept: "application/json"
+      }}
+    )
+    .then(r => r.json())
+    .then(console.log)
+  }
+
+
+  render() {
+    return <div>CatContainer</div>
+  }
 }
 
 export default CatContainer
