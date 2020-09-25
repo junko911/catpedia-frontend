786b587bd2e9451acab06ee83828fcdd727ea69e
junko911 <junkotahara911@gmail.com>
2020-09-24 22:12:28 -0400

    Delete unneeded lines

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import './App.css';
-import Login from './Components/Login'
-import Signup from './Components/Signup'
 import { BrowserRouter, Route } from 'react-router-dom'
-import './App.css'
 import NavBar from "./Components/Navbar"
 import Login from './Components/Login'
 import Signup from './Components/Signup'
