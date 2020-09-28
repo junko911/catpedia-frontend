14100cc71c99061e6c3f00c9bff197005d76fcb0
junko911 <junkotahara911@gmail.com>
2020-09-28 17:10:45 -0400

    Set up file upload form

--- src/Containers/ImageUpload.js
+++ src/Containers/ImageUpload.js
@@ -1,7 +1,23 @@
 import React from 'react'
+import Dropzone from 'react-dropzone'
 
 const ImageUpload = () => {
-  return <div>Upload image</div>
+
+  return (
+    <div className="form-container">
+      <h2>Upload a .jpg or .png cat image</h2>
+      <Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)}>
+        {({ getRootProps, getInputProps }) => (
+          <section>
+            <div {...getRootProps(({ className: 'dropzone' }))}>
+              <input {...getInputProps()} />
+              <p>Drag 'n' drop some files here, or click to select files</p>
+            </div>
+          </section>
+        )}
+      </Dropzone>
+    </div>
+  )
 }
 
 export default ImageUpload
